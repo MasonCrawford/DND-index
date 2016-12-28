e4cbfe695755d38ea5ca3b9c1998db6b7d93c44c
MasonCrawford <masoncrawford1994@gmail.com>
2016-12-28 17:12:58 +0800

    work on class page
    starting work on class page form, not happy with the way i am
    displaying to info going to work on a better way but can’t think of one
    right now

--- assets/js/3-pages/classpage.js
+++ assets/js/3-pages/classpage.js
@@ -6,7 +6,10 @@ classpage.controller('classlistcon', function ($scope, $http) {
     console.log($scope.dndclasses);
   });
 });
-
+classpage.controller('classform', function ($scope) {
+    console.log("this is a test");
+  });
+});
 classpage.filter('suboveride', function () {
   return function(main, sub){
     if (sub == "" | sub == undefined) {
@@ -15,7 +18,26 @@ classpage.filter('suboveride', function () {
     return sub;
   };
   });
-  // classpage.filter('castersen', function () {
+  classpage.filter('castersen', function () {
+    return function(input){
+      console.log(input);
+      if (input == ""| input == "No") {
+        console.log("ented");
+        return "is not a caster"
+      }
+      else if (input == "Ritual") {
+        return "is a Ritual caster"
+      }
+      else if (input == "yes") {
+        return "is a spell caster"
+      }
+      else if (input == "More") {
+        return "is more of a spell caster than outhere sub classes"
+      }
+      return input;
+    };
+  });
+  // classpage.filter('preparedsen', function () {
   //   return function(input){
   //     console.log(input);
   //     if (input == ""| input == "No") {
@@ -33,18 +55,4 @@ classpage.filter('suboveride', function () {
   //     }
   //     return input;
   //   };
-  //   });
-classpage.filter('makesen', function(){
-  return function(input, propaty){
-    // console.log(input);
-    // console.log("propaty",propaty);
-        if (input == ""| input == "No") {
-          return "is not a " & propaty
-        }
-        else if (input == "yes") {
-          return "is a "& propaty
-        }
-        return "";
-
-  };
-});
+  // });
